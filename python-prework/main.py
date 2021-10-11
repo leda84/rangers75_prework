2e0ac9f0bba67e0fc83380956d51c9747186d61b
leda84 <laura.dejesus929@gmail.com>
2021-10-11 14:03:06 -0500

    made more practice changes

--- python-prework/main.py
+++ python-prework/main.py
@@ -64,3 +64,16 @@ def is_consecutive2(a_list):
 
 is_consecutive2([3,4,5,6])
 is_consecutive2([1,5,2,3])
+
+#making more practices changes
+def get_formatted_name(first_name, last_name, middle_name = ' '):
+    """Return a full name, neatly formatted"""
+    if middle_name:
+        full_name = first_name + ' ' + middle_name + ' ' + last_name
+    else:
+        full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
+
