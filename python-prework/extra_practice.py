b78bee7432159889ec0a997a6757c3d4d1da01de
leda84 <laura.dejesus929@gmail.com>
2021-10-11 13:42:14 -0500

    practice run pushing to github

---
+++ python-prework/extra_practice.py
@@ -0,0 +1,6 @@
+#This is extra practice to make sure we are comfortable uploading onto github
+def hey_there(name):
+    """this is a practice function"""
+    return print('Hello ' + name + '!')
+
+hey_there('Laura')
